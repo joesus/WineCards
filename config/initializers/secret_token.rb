fb2947192f345dcc8513c2dc0b10e9653cb69356
Joesus <joesusnick@gmail.com>
2013-08-26 13:17:04 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WineCards::Application.config.secret_key_base = 'fcf643f3ca6adfc3f3600c26bd8da63382975f479ef18ded81a207644e7d04031c77a2dce3f8c39d54df719b9aeec54c23e7ed61517eea2b7391464be1ee0db9'
