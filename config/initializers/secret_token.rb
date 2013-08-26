15dc08c50b37f725e303d1e184ec9991df007d07
Joesus <joesusnick@gmail.com>
2013-08-26 13:37:33 -0600

    Added secure_token method

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,15 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-WineCards::Application.config.secret_key_base = 'fcf643f3ca6adfc3f3600c26bd8da63382975f479ef18ded81a207644e7d04031c77a2dce3f8c39d54df719b9aeec54c23e7ed61517eea2b7391464be1ee0db9'
+# Be sure to restart your server when you modify this file.  # Your secret key is used for verifying the integrity of signed cookies. # If you change this key, all old signed cookies will become invalid!  # Make sure the secret is at least 30 characters and all random, # no regular words or you'll be exposed to dictionary attacks. # You can use `rake secret` to generate a secure secret key.  # Make sure your secret_key_base is kept private # if you're sharing your code publicly. 
+require 'securerandom'
+  def secure_token
+   token_file = Rails.root.join('.secret')
+   if File.exist?(token_file)
+     # Use the existing token.
+     File.read(token_file).chomp
+   else
+     # Generate a new token and store it in token_file.
+     token = SecureRandom.hex(64)
+     File.write(token_file, token)
+     token	
+   end
+ end
+WineCards::Application.config.secret_key_base = secure_token
