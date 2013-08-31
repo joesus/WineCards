601d6499c2fae959ad27ea2cbc6d9d00f3621f7c
Joesus <joesusnick@gmail.com>
2013-08-30 18:13:47 -0600

    Wine model added with tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130829224415) do
+ActiveRecord::Schema.define(version: 20130830205333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,5 +28,15 @@ ActiveRecord::Schema.define(version: 20130829224415) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
+
+  create_table "wines", force: true do |t|
+    t.string   "varietal"
+    t.string   "country"
+    t.integer  "vintage"
+    t.string   "description"
+    t.integer  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -5,8 +5,16 @@ FactoryGirl.define do
 		password	"foobar"
 		password_confirmation "foobar"
 	end
+
+	factory :wine do
+		varietal		"grape"
+		country			"Wineland"
+		vintage			2000
+		description "tasty"
+		price				{ 10 + rand(100) }
+	end
 end
 
 # By passing :user to the factory we tell the factory that the subsequent
-# definition is for a User model object. 
+# definition is for a User model object, same with :wine. 
 
