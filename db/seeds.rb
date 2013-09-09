a19c47b8f892c71456349c4cd3693dc8ae50b516
Joesus <joesusnick@gmail.com>
2013-09-09 15:22:23 -0600

    Added Producer to Wines

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = User.create(name:"Admin", email:"joesusnick@gmail.com", 
+	password:"foobar", password_confirmation:"foobar", admin: true)
